56b9e136eaf05c7b013fa59dae8a3f8ea6a4dbd5
unknown <ghinea.alexandru.george@gmail.com>
2019-10-20 13:09:33 +0300

    mvc pie shop

---
+++ Models/MockCategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class MockCategoryRepository: ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+            new List<Category>
+            { 
+                new Category{CategoryId=1, CategoryName="Fruit pies", Description="All-fruit"},
+                new Category{CategoryId=2, CategoryName="Cheese", Description="Bread"}
+            };
+    }
+}
---
+++ Models/MockPieRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class MockPieRepository : IPieRepository
+    {
+        private readonly ICategoryRepository _categoryRepository = new MockCategoryRepository();
+
+        public IEnumerable<Pie> AllPies =>
+            new List<Pie>
+            {
+                new Pie {PieId = 1, Name = "Cheese Pie", Price=15.95M, ShortDescription = "good", LongDescription = "very long desc", AllergyInformation = "none", CategoryId = 1}
+            
+            
+            };
+      
+        public IEnumerable<Pie> PiesOfTheWeek { get;  }
+
+        public Pie GetPieById(int pieId)
+        {
+            return AllPies.FirstOrDefault(p => p.PieId == pieId);
+        }
+    }
+}
