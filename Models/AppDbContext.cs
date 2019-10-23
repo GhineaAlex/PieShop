ea91c9ec44de4a10dad7a1a25008e5e69be201a5
unknown <ghinea.alexandru.george@gmail.com>
2019-10-23 19:43:45 +0300

    database connection

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -14,5 +14,46 @@ namespace WebApplication1.Models
 
         public DbSet<Pie> Pies { get; set; }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(new Category { CategoryId= 1, CategoryName = "Fruit pies"});
+            modelBuilder.Entity<Category>().HasData(new Category { CategoryId = 2, CategoryName = "Cheese cakes" });
+            modelBuilder.Entity<Category>().HasData(new Category { CategoryId = 3, CategoryName = "Seasonal pies" });
+
+            modelBuilder.Entity<Pie>().HasData(new Pie
+            {
+                PieId = 1,
+                Name = "Apple Pie",
+                Price = 13,
+                ShortDescription = "Best applie pie",
+                LongDescription = "Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. ",
+                CategoryId = 1,
+                ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/strawberrypie.jpg",
+                InStock = true,
+                IsPieOfTheWeek = true,
+                ImageThumbnailUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/strawberrypiesmall.jpg",
+                AllergyInformation = ""
+
+            });
+
+            modelBuilder.Entity<Pie>().HasData(new Pie
+            {
+                PieId = 2,
+                Name = "Cheese cakes",
+                Price = 16,
+                ShortDescription = "Best cheese cakes",
+                LongDescription = "Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. ",
+                CategoryId = 2,
+                ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/strawberrypie.jpg",
+                InStock = true,
+                IsPieOfTheWeek = true,
+                ImageThumbnailUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/strawberrypiesmall.jpg",
+                AllergyInformation = ""
+
+            });
+        }
     }
 }
--- Models/Pie.cs
+++ Models/Pie.cs
@@ -12,13 +12,13 @@ namespace WebApplication1.Models
         public string ShortDescription { get; set; }
         public string LongDescription { get; set; }
         public string AllergyInformation { get; set; }
-        public decimal Price { get; set; }
+        public int Price { get; set; }
         public string ImageUrl { get; set; }
         public string ImageThumbnailUrl { get; set; }
         public bool IsPieOfTheWeek { get; set; }
         public bool InStock { get; set; }
         public int CategoryId { get; set; }
-
         public Category Category { get; set; }
+        public string Notes { get; set; }
     }
 }
